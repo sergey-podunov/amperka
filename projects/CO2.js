c49d1523160e0f69aaf1a2f9d98541896d4decab
Sergey Podunov <s.podunov@hh.ru>
2020-08-04 21:24:20 +0300

    CO2 with display

--- projects/CO2.js
+++ projects/CO2.js
@@ -1,15 +1,19 @@
+SPI2.setup({baud: 9600, mosi: B15, miso: B14, sck: B13});
+let quadDisplay = require('@amperka/quaddisplay2').connect({spi: SPI2, cs: P9});
+
 var gasSensor = require('@amperka/gas-sensor').connect({
   dataPin: A0, // разъём SVG
   heatPin: P10, // разъём GHE
   model: 'MQ135'
 });
- 
+
 gasSensor.preheat(function() {
-  var basePpm = gasSensor.calibrate();
-  print('PPM base in current environment: ', basePpm);
- 
+  var r0 = gasSensor.calibrate();
+  print('r0 = ', r0);
+
   setInterval(function() {
-    var val = gasSensor.read('CO2');
+    var val = Math.round(gasSensor.read('CO2'));
     print('LPG PPM =', val);
+    quadDisplay.display(val, true);
   }, 1000);
 });
