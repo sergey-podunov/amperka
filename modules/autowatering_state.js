c4e2cd08c1cb6b24b348c89ffd67885b85921ce4
Sergey Podunov <s.podunov@hh.ru>
2020-10-18 15:40:42 +0300

    check in interval

--- modules/autowatering_state.js
+++ modules/autowatering_state.js
@@ -2,15 +2,25 @@ var AutowateringState = function (ops) {
     this._start = ops.start;
     this._is_no_water = ops.no_water;
     this._moisture_low = ops.moisture_low;
+    this._active = false;
 }
 
-AutowateringState.prototype._get_state = function() {
-    return { start: this._start, no_water: this._is_no_water, moisture_low: this._moisture_low };
+AutowateringState.prototype._get_state = function () {
+    return { active: this._active, start: this._start, no_water: this._is_no_water, moisture_low: this._moisture_low };
 }
 
 AutowateringState.prototype.change_start = function (new_val) {
     this._start = new_val;
     this.emit('change', this._get_state());
+}
+
+AutowateringState.prototype.change_active = function (new_val) {
+    this._active = new_val;
+    this.emit('change', this._get_state());
+}
+
+AutowateringState.prototype.is_active = function () {
+    return this._active;
 }
 
 AutowateringState.prototype.change_water_level = function (new_val) {
--- projects/autowatering.js
+++ projects/autowatering.js
@@ -8,22 +8,33 @@ const DWEET_NAME = external_conf.dweet_name;
 
 const SEND_DWEET_INTERVAL_MS = 3000;
 const MOISTURE_CHECK_INTERVAL_MS = 200;
+const START_ACTIVE_INTERVAL_MS = 24 * 60 * 60 * 1000;
+const ACTIVE_TIMEOUT_MS = 20 * 1000;
 
 const MOISTURE_PIN = P6;
+const MOISTURE_POWER_PIN = A4;
+
 const PUMP_PIN = P11;
 const WATER_LEVEL_PIN = P9;
 
 var config = {
-    start: false,
+    start: true,
     hyst: {high: 0.4, highLag: 2, low: 0.3, lowLag: 2}
 };
 
-var equal_functions = require('equal_functions').create();
+function getMoisture() {
+    analogWrite(MOISTURE_POWER_PIN, 1);
+    let moisture = analogRead(MOISTURE_PIN);
+    analogWrite(MOISTURE_POWER_PIN, 0);
+    return moisture;
+}
+
+var wifi_ready = false;
 
 var dweet = require('@amperka/dweetio').connect(DWEET_NAME);
 
-var hyst = require('hysteresis').create(config.hyst);
-var is_moisture_level_low = analogRead(MOISTURE_PIN) <= config.hyst.low;
+var hyst_moisture = require('hysteresis').create(config.hyst);
+var is_moisture_level_low = getMoisture() <= config.hyst.low;
 
 var pump = require('@amperka/power-control').connect(PUMP_PIN);
 pump.turnOff();
@@ -55,18 +66,14 @@ function convert_conf_to_dweet(conf, prefix) {
             }
         }
     }
-    conf_for_dweet.is_able_to_work = is_able_to_work();
+    conf_for_dweet.is_able_to_work = config.start && !is_no_water;
     conf_for_dweet.is_no_water = is_no_water;
     conf_for_dweet.water_level = water_level.read() === 'up' ? 1 : 0;
     conf_for_dweet.pump = pump_is_on ? 1 : 0;
-    conf_for_dweet.moisture = analogRead(MOISTURE_PIN);
+    conf_for_dweet.moisture = getMoisture();
     conf_for_dweet.moisture_low = is_moisture_level_low ? 1 : 0;
 
     return conf_for_dweet;
-}
-
-function is_changed(conf_old, conf_new) {
-    return !equal_functions.deep_equal(conf_old, conf_new);
 }
 
 function pump_on() {
@@ -83,7 +90,7 @@ function pump_off() {
     pump.turnOff();
 }
 
-function wifiReady() {
+function wifi_ready_callback() {
     setInterval(function () {
         var conf_for_dweet = convert_conf_to_dweet(config, 'conf_');
         dweetSend(conf_for_dweet);
@@ -94,30 +101,41 @@ var wifi = require('@amperka/wifi').setup(function (err) {
     if (err) print(err);
 
     wifi.connect(SSID, PASSWORD, function (err) {
-        if (err) print(err);
+        if (err) {
+            print(err);
+        } else {
+            wifi_ready = true;
+        }
 
-        print('wifi ready');
+        if (wifi_ready) {
+            print('wifi ready');
+            print('Click this link', dweet.follow());
+        }
 
         LED1.write(true);
-        print('Click this link', dweet.follow());
-        wifiReady();
+
+        wifi_ready_callback();
     });
 });
 
-function is_able_to_work() {
-    return config.start && !is_no_water;
-}
-
 setInterval(function() {
-    hyst.push(analogRead(MOISTURE_PIN));
+    if (state.is_active()) {
+        var moisture = getMoisture();
+        hyst_moisture.push(moisture);
+        print("hyst_moisture.state " + hyst_moisture._state, ", hyst_moisture.stable " + hyst_moisture._stable,
+            "moisture " + moisture);
+    }
 }, MOISTURE_CHECK_INTERVAL_MS);
 
-hyst.on('change', function (level) {
-    print('hyst change - state: ' + level);
-    state.change_moisture_level(level);
+
+hyst_moisture.on('change', function (level) {
+    if (state.is_active()) {
+        print('hyst change - state: ' + level);
+        state.change_moisture_level(level);
+    }
 });
 
-hyst.on('change', function (level) {
+hyst_moisture.on('change', function (level) {
     is_moisture_level_low = level === 'low';
 });
 
@@ -137,21 +155,41 @@ water_level.on('up', function () {
 
 state.on('change', function (new_state) {
     print('change state: ' + JSON.stringify(new_state));
-    if (new_state.start && !new_state.no_water && new_state.moisture_low) {
+    if (new_state.active && new_state.start && !new_state.no_water && new_state.moisture_low) {
         pump_on();
         return;
     }
 
-    if (!new_state.start || new_state.no_water || !new_state.moisture_low) {
+    if (!new_state.active || !new_state.start || new_state.no_water || !new_state.moisture_low) {
+        if (new_state.active && pump_is_on) {
+            state._active = false;
+        }
+
         pump_off();
     }
 });
 
 function dweetSend(data) {
-    let ledInitialState = LED1.read();
-    LED1.write(!ledInitialState);
+    if (wifi_ready) {
+        let ledInitialState = LED1.read();
+        LED1.write(!ledInitialState);
 
-    dweet.send(data, function () {
-        LED1.write(ledInitialState);
-    });
+        dweet.send(data, function () {
+            LED1.write(ledInitialState);
+        });
+    }
 }
+
+function main_cycle() {
+    print('change_active = true');
+    state.change_active(true);
+
+    setTimeout(function() {
+        print('change_active = false');
+        state.change_active(false);
+    }, ACTIVE_TIMEOUT_MS);
+}
+
+main_cycle();
+
+setInterval(main_cycle, START_ACTIVE_INTERVAL_MS);
